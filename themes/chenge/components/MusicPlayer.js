a06eab782b75d087e84c92a23667763afb0890d2
chenge_home <chenyq.thu@gmail.com>
2024-02-25 02:11:53 +0800

    修复音乐播放器错误

--- themes/chenge/components/MusicPlayer.js
+++ themes/chenge/components/MusicPlayer.js
@@ -48,7 +48,7 @@ const MusicPlayer = () => {
   useEffect(() => {
     const loadResourcesAndInitPlayer = async () => {
       if (!document.querySelector('link[href="/css/player.css"]')) {
-        loadExternalResource('css/player.css', 'css');
+        loadExternalResource('/css/player.css', 'css');
       }
       if (!document.querySelector('script[src="/js/mediaPlayer.js"]')) {
         await loadExternalResource('/js/mediaPlayer.js', 'js');
