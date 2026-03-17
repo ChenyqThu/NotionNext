641885b05ae36277da6cf77cf7ecc767815cf86f
陈源泉 <chenyuanquan@chenyuanquandeMac-mini.local>
2026-03-16 18:15:43 -0700

    Vendor unstable frontend assets

--- components/Fireworks.js
+++ components/Fireworks.js
@@ -17,10 +17,7 @@ const Fireworks = () => {
   useEffect(() => {
     // 异步加载
     function loadFireworks() {
-      loadExternalResource(
-        'https://cdnjs.snrat.com/ajax/libs/animejs/3.2.1/anime.min.js',
-        'js'
-      ).then(() => {
+      loadExternalResource('/js/anime.min.js', 'js').then(() => {
         loadExternalResource('/js/fireworks.js', 'js').then(() => {
           if (window.anime && window.createFireworks) {
             window.createFireworks({
